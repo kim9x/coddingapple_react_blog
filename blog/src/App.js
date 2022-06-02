98ffc0b81818866d8c83e4b3a35b87390fa05715
Kim9xd <kim9xd@gmail.com>
2022-06-02 22:49:56 +0900

    * React 리액트 기초부터 쇼핑몰 프로젝트까지!  - array, object state 변경하는 법   * 정리 *   리액트에서 array/object state를 수정하고 싶으면   독립적인 카피본을 만들어서 수정하는게 좋습니다.   [...기존state]   {...기존state}   이렇게 하면 독립적인 카피가 하나 생성됩니다.

--- blog/src/App.js
+++ blog/src/App.js
@@ -7,7 +7,7 @@ import { useState } from 'react';
 function App() {
 
   let post = '강남 우동 맛집';
-  let [글제목, b] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
 
   function 함수() {
@@ -19,9 +19,21 @@ function App() {
       <div className="black-nav">
         <h4>{post}</h4>
       </div>
+
+      <button onClick={() => {
+        // let copy = [...글제목];
+        // copy[0] = '여자코트 추천';
+        // 글제목변경(copy);
+
+        let copy = [...글제목];
+        copy.sort();
+        글제목변경(copy);
+
+
+      }}>글수정</button>
+
       <div className="list">
         <h4>{글제목[0]} <span onClick={() => { 따봉변경(따봉 + 1) }}>👍</span> {따봉}  </h4>
-        {/* <button onClick={() => { b(['여자 코트 추천', '강남 우동맛집', '파이썬 독학']) }}>이름 변경</button> */}
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
