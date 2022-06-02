077088bea0104258de76af64b99a6c4ef8f39327
Kim9xd <kim9xd@gmail.com>
2022-06-02 21:49:29 +0900

    * React 리액트 기초부터 쇼핑몰 프로젝트까지!  - 중요한 데이터는 변수말고 state에 담습니다   자주변경될 것 같은 데이터들은 state에 저장했다가 html에 {데이터바인딩} 해놓으십시오   1. 변경할 일이 없는 데이터들   2. 굳이 html에 표기가 필요없는 데이터들은 그냥 변수에 저장해도 됩니다.

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,17 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '강남 우동 맛집';
+  let [글제목, b] = useState('남자 코트 추천');
+  let [logo, setLogo] = useState('ReactBlog');
+
+  // const [first, setfirst] = useState(second);
+
+  let num = [1, 2];
+
+  let [a, c] = [1, 2];
+
+  // let a = num[0];
+  // let c = num[1];
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{ color: 'red', fontSize: '16px' }}>블로그임</h4>
+        <h4>{post}</h4>
       </div>
-      <h4>{post}</h4>
-
+      <div className="list">
+        <h4>{글제목}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div >
   );
 }
