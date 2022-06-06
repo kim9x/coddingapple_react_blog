b48c48f016a7654b6eb9de9258757abdb4fc85e3
kim9x <kim9xd@gmail.com>
2022-06-06 19:36:15 +0900

    * React 리액트 기초부터 쇼핑몰 프로젝트까지!  - map : 많은 div들을 반복문으로 줄이고 싶은 충동이 들 때   * 정리 *   (참고) 일반 for 반복문을 쓰고싶다면   1. html들을 담아둘 array 자료를 하나 만들어줍니다.   2. 일반 for 반복문을 이용해서 반복문을 돌림   3. 반복될 때 마다 array자료에 <div> 하나씩 추가해줍니다.   4. 원하는 곳에서 {array자료} 사용하면 됩니다.

--- blog/src/App.js
+++ blog/src/App.js
@@ -8,8 +8,13 @@ function App() {
 
   let post = '강남 우동 맛집';
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
+  // let [따봉, 따봉변경] = useState(0);
   let [modal, setModal] = useState(false);
+
+  [1, 2, 3].map(function(a) {
+    console.log(a);
+  })
 
   function modalSwitch() {
     if (modal == true) {
@@ -17,6 +22,14 @@ function App() {
     } else {
       setModal(true);
     }
+  }
+
+  function clickDdabong(i) {
+    console.log("i :", i);
+    let copy = [...따봉];
+        // copy.sort();
+        copy[i] = copy[i] + 1
+        따봉변경(copy);
   }
 
   return (
@@ -37,7 +50,7 @@ function App() {
 
       }}>글수정</button>
 
-      <div className="list">
+      {/* <div className="list">
         <h4>{글제목[0]} <span onClick={() => { 따봉변경(따봉 + 1) }}>👍</span> {따봉}  </h4>
         <p>2월 17일 발행</p>
       </div>
@@ -48,7 +61,17 @@ function App() {
       <div className="list">
         <h4 onClick={() => { setModal(!modal) }}>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
-      </div>
+      </div> */}
+
+      {
+        글제목.map(function(a, i) {
+          return (<div className="list" key={i}>
+            {/* <h4>{ a }</h4> */}
+            <h4>{ 글제목[i] } <span onClick={() => { clickDdabong(i) }}>👍</span> {따봉[i]} </h4>
+            <p>2월 17일 발행</p>
+            </div>)
+        })
+      }
 
       {
         modal == true ? <Modal/> : null
