aec6953898a065100ff182f67e274f8a881c2b76
Kim9xd <kim9xd@gmail.com>
2022-06-09 23:40:11 +0900

    * React 리액트 기초부터 쇼핑몰 프로젝트까지!  - Part 2 : 쇼핑몰 프로젝트   - 새로운 프로젝트 생성 & Bootstrap 사용하기   - 이미지 넣는 법 & public 폴더 이용하기

---
+++ shop/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+import bg from './img/bg.png'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Finance</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Stock</Nav.Link>
+            <Nav.Link href="#features">Real estate</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='main-bg' style={{ backgroundImage: 'url(' + bg + ')' }}></div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src={process.env.PUBLIC_URL + '/logo192.png'} width='80%' />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+
+          </div>
+          <div className="col-md-4">
+            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width='80%' />
+            <h4>상품명</h4>
+            <p>상품설명</p></div>
+          <div className="col-md-4">
+            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width='80%' />
+            <h4>상품명</h4>
+            <p>상품설명</p></div>
+        </div>
+      </div>
+    </div >
+  );
+}
+
+export default App;
