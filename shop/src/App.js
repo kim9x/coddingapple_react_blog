2d6bcc113c31f1f59e95af794957f28653e4a604
Kim9xd <kim9xd@gmail.com>
2022-06-16 23:11:03 +0900

    * React 리액트 기초부터 쇼핑몰 프로젝트까지!  - Part 2 : 쇼핑몰 프로젝트   - 리액트 라우터 2 : navigate, nested routes, outlet

--- shop/src/App.js
+++ shop/src/App.js
@@ -4,13 +4,14 @@ import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import bg from './img/bg.png'
 import { useState } from 'react';
 import data from './data.js';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 import Detail from './routes/Detail.js';
 
 
 function App() {
 
   let [shoes] = useState(data);
+  let navigate = useNavigate();
 
   return (
     <div className="App">
@@ -18,8 +19,8 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">Finance</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href='/'>홈</Nav.Link>
-            <Nav.Link href="/detail">상세페이지</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/') }}>Home</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/detail') }}>Detail</Nav.Link>
             {/* <Link to='/'>홈</Link>
             <Link to='/detail'>상세페이지</Link> */}
           </Nav>
@@ -44,11 +45,42 @@ function App() {
               </div>
             </div></>} />
         <Route path='/detail' element={<Detail />} />
+        <Route path='/about' element={<About />} >
+          <Route path='member' element={<div>멤버임</div>} />
+          <Route path='location' element={<About />} />
+        </Route>
+        {/* <Route path='/about/member' element={<About />} />
+        <Route path='/about' element={<About />} /> */}
+
+        <Route path='/event' element={<EventPage />} >
+          <Route path='one' element={<div>첫 주문 시 양배추즙 서비스</div>} />
+          <Route path='two' element={<div>생일기념 쿠폰받기</div>} />
+        </Route>
       </Routes>
 
 
     </div >
   );
+}
+
+function EventPage() {
+  return (
+    <div>
+      <h4>
+        오늘의 이벤트
+      </h4>
+      <Outlet></Outlet>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
 }
 
 function Card(props) {
