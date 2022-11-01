6441c59bd06b53ded8b9025e5ade8baac6bbb575
Kim9xd <kim9xd@gmail.com>
2022-11-02 00:00:13 +0900

    장바구니 페이지 만들기 & Redux 1 : Redux Toolkit 설치

--- shop/src/App.js
+++ shop/src/App.js
@@ -7,6 +7,7 @@ import data from './data.js';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 import Detail from './routes/Detail.js';
 import axios from 'axios'
+import Cart from './routes/Cart.js';
 
 export let Context1 = createContext();
 
@@ -90,6 +91,7 @@ function App() {
             <Detail shoes={shoes} />
           </Context1.Provider>
         } />
+        <Route path="/cart" element={<Cart />} />
         <Route path='/about' element={<About />} >
           <Route path='member' element={<div>멤버임</div>} />
           <Route path='location' element={<About />} />
