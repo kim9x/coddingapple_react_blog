f0c3883f1c86d5e17ee7659b353f50790e7fe1e2
Kim9xd <kim9xd@gmail.com>
2022-11-08 21:34:49 +0900

    성능개선 1 : 개발자도구 & lazy import

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,12 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import bg from './img/bg.png'
-import { createContext, useEffect, useState } from 'react';
+import { lazy, Suspense, createContext, useEffect, useState } from 'react';
 import data from './data.js';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
-import Detail from './routes/Detail.js';
 import axios from 'axios'
-import Cart from './routes/Cart.js';
+
+// import Detail from './routes/Detail.js';
+// import Cart from './routes/Cart.js';
+
+// 처음 진입 시 큰 자바스크립트 파일을
+// 불러오기 때문에 느린데
+// 아래처럼 lazy로 import를 해주면 따로따로 필요할 때
+// import 되기 때문에
+// 처음 로딩 속도가 느려지는 것을 방지해줄 수 있음.
+// 그러나 결국 아래 Component가 로딩될 때는
+// 느려지는 현상이 발생할 수 있음.
+const Detail = lazy(() => import('./routes/Detail.js'));
+const Cart = lazy(() => import('./routes/Cart.js'));
 
 export let Context1 = createContext();
 
@@ -50,78 +61,80 @@ function App() {
       </Navbar>
 
 
+      <Suspense fallback={<div>로딩중임</div>}>
+        <Routes>
+          <Route path="/" element={
+            <><div className='main-bg' style={{ backgroundImage: 'url(' + bg + ')' }}></div>
 
-      <Routes>
-        <Route path="/" element={
-          <><div className='main-bg' style={{ backgroundImage: 'url(' + bg + ')' }}></div>
-
-            <div className="container">
-              <div className="row">
-                {
-                  shoes.map(function (a, i) {
-                    return (
-                      <Card shoes={shoes[i]} i={i} />
-                    )
-                  })
-                }
+              <div className="container">
+                <div className="row">
+                  {
+                    shoes.map(function (a, i) {
+                      return (
+                        <Card shoes={shoes[i]} i={i} />
+                      )
+                    })
+                  }
+                </div>
               </div>
-            </div>
-            <button onClick={() => {
-              // 로딩중UI 띄우기~
-              axios.get('https://codingapple1.github.io/shop/data2.json')
-                .then((datas) => {
-                  // console.log(datas.data)
-                  let copy = [...shoes, ...datas.data];
-                  setShoes(copy);
-                  // console.log(shoes)
-                  // 로딩중UI 숨기기~
-                })
-                .catch(() => {
-                  console.log('실패함ㅅㄱ')
-                  // 로딩중UI 숨기기~
-                })
+              <button onClick={() => {
+                // 로딩중UI 띄우기~
+                axios.get('https://codingapple1.github.io/shop/data2.json')
+                  .then((datas) => {
+                    // console.log(datas.data)
+                    let copy = [...shoes, ...datas.data];
+                    setShoes(copy);
+                    // console.log(shoes)
+                    // 로딩중UI 숨기기~
+                  })
+                  .catch(() => {
+                    console.log('실패함ㅅㄱ')
+                    // 로딩중UI 숨기기~
+                  })
 
 
-              // ajax로 post 요청 보내기
-              axios.post('/test', { name: 'kim' })
+                // ajax로 post 요청 보내기
+                axios.post('/test', { name: 'kim' })
 
-              // 한번에 2개의 url에 ajax 요청하기
-              Promise.all([axios.get('/url1'), axios.get('/url2')])
-                .then(() => {
+                // 한번에 2개의 url에 ajax 요청하기
+                Promise.all([axios.get('/url1'), axios.get('/url2')])
+                  .then(() => {
 
-                })
-              // 아래처럼 따옴표로 감싼 후 요청 시
-              // array, object도 주고받기 가능 (json 형식임)
-              // axios가 json 형식을 자동변환 해줌.
-              // "{"name" : "kim"}"
+                  })
+                // 아래처럼 따옴표로 감싼 후 요청 시
+                // array, object도 주고받기 가능 (json 형식임)
+                // axios가 json 형식을 자동변환 해줌.
+                // "{"name" : "kim"}"
 
-              // JS 기본 문법인 fetch로 요청하기
-              fetch('https://codingapple1.github.io/shop/data2.json')
-                // fetch를 사용하면 json을 array 혹은 object로 변환하는 코드 필요
-                .then(datas => datas.json)
-                .then(data => { })
-            }}>더보기</button>
-          </>
-        } />
-        <Route path='/detail/:id' element={
-          <Context1.Provider value={{ 재고, shoes }}>
-            <Detail shoes={shoes} />
-          </Context1.Provider>
-        } />
-        <Route path="/cart" element={<Cart />} />
-        <Route path='/about' element={<About />} >
-          <Route path='member' element={<div>멤버임</div>} />
-          <Route path='location' element={<About />} />
-        </Route>
-        {/* <Route path='/about/member' element={<About />} />
+                // JS 기본 문법인 fetch로 요청하기
+                fetch('https://codingapple1.github.io/shop/data2.json')
+                  // fetch를 사용하면 json을 array 혹은 object로 변환하는 코드 필요
+                  .then(datas => datas.json)
+                  .then(data => { })
+              }}>더보기</button>
+            </>
+          } />
+          <Route path='/detail/:id' element={
+            <Context1.Provider value={{ 재고, shoes }}>
+
+              <Detail shoes={shoes} />
+
+            </Context1.Provider>
+          } />
+          <Route path="/cart" element={<Cart />} />
+          <Route path='/about' element={<About />} >
+            <Route path='member' element={<div>멤버임</div>} />
+            <Route path='location' element={<About />} />
+          </Route>
+          {/* <Route path='/about/member' element={<About />} />
         <Route path='/about' element={<About />} /> */}
 
-        <Route path='/event' element={<EventPage />} >
-          <Route path='one' element={<div>첫 주문 시 양배추즙 서비스</div>} />
-          <Route path='two' element={<div>생일기념 쿠폰받기</div>} />
-        </Route>
-      </Routes>
-
+          <Route path='/event' element={<EventPage />} >
+            <Route path='one' element={<div>첫 주문 시 양배추즙 서비스</div>} />
+            <Route path='two' element={<div>생일기념 쿠폰받기</div>} />
+          </Route>
+        </Routes>
+      </Suspense>
 
     </div >
   );
